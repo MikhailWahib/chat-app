3bdfef9e7bd5296684c2c8f853f6f9a8fca694a0
MikhailWahib <mikhailwahib1@gmail.com>
2024-06-29 14:11:42 +0300

    Refactor

--- main.go
+++ main.go
@@ -5,71 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-type Message struct {
-	Username string `json:"username"`
-	Message  string `json:"message"`
-}
-
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message)
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-
-	clients[conn] = true
-
-	for {
-		var msg Message
-		err := conn.ReadJSON(&msg)
-		if err != nil {
-			fmt.Println(err)
-			delete(clients, conn)
-			return
-		}
-
-		broadcast <- msg
-	}
-}
-
-func handleMessages() {
-	for {
-		msg := <-broadcast
-
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				fmt.Println(err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-	}
-}
 
 var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
 	flag.Parse()
+	room := newRoom()
+
+	go room.run()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte{'s', 's', 's'})
+		w.Write([]byte("Server running"))
 	})
-	http.HandleFunc("/ws", handleConnections)
 
-	go handleMessages()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(room, w, r)
+	})
 
 	fmt.Println("Server running on", *addr)
 	err := http.ListenAndServe(*addr, nil)
---
+++ room.go
@@ -0,0 +1,40 @@
+package main
+
+type Room struct {
+	clients    map[*Client]bool
+	broadcast  chan Message
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newRoom() *Room {
+	return &Room{
+		broadcast:  make(chan Message),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (r *Room) run() {
+	for {
+		select {
+		case client := <-r.register:
+			r.clients[client] = true
+		case client := <-r.unregister:
+			if _, ok := r.clients[client]; ok {
+				delete(r.clients, client)
+				close(client.send)
+			}
+		case message := <-r.broadcast:
+			for client := range r.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(r.clients, client)
+				}
+			}
+		}
+	}
+}
