f5c8ca088b55fc0633201e5e091cb28922e808aa
MikhailWahib <mikhailwahib1@gmail.com>
2024-07-02 20:00:21 +0300

    feat(server): add room's members count to the GET /rooms endpoint

--- server/room.go
+++ server/room.go
@@ -1,24 +1,26 @@
 package main
 
 type Room struct {
-	ID         string `json:"id"`
-	Name       string `json:"name"`
-	password   string
-	clients    map[*Client]bool
-	broadcast  chan Message
-	register   chan *Client
-	unregister chan *Client
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	ClientsCount int    `json:"members"`
+	password     string
+	clients      map[*Client]bool
+	broadcast    chan Message
+	register     chan *Client
+	unregister   chan *Client
 }
 
 func newRoom(name string, pwd string) *Room {
 	return &Room{
-		ID:         GenerateId(5),
-		Name:       name,
-		password:   pwd,
-		broadcast:  make(chan Message),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
+		ID:           GenerateId(5),
+		Name:         name,
+		ClientsCount: 0,
+		password:     pwd,
+		broadcast:    make(chan Message),
+		register:     make(chan *Client),
+		unregister:   make(chan *Client),
+		clients:      make(map[*Client]bool),
 	}
 }
 
@@ -27,8 +29,10 @@ func (r *Room) run() {
 		select {
 		case client := <-r.register:
 			r.clients[client] = true
+			r.ClientsCount += 1
 		case client := <-r.unregister:
 			if _, ok := r.clients[client]; ok {
+				r.ClientsCount -= 1
 				delete(r.clients, client)
 				close(client.send)
 			}
