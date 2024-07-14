5a8b81cd8887f932735d0b667438909e92ad1b20
Mikhail <mikhailwahib1@gmail.com>
2024-07-14 15:35:29 +0300

    server: add /api/index.go for vercel

---
+++ server/api/index.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"chat-app/api/router"
+	"chat-app/ws"
+)
+
+func main() {
+	h := ws.NewHub()
+	wsHandler := ws.NewHandler(h)
+	go h.Run()
+
+	router.InitRouter(wsHandler)
+	router.Run()
+}
