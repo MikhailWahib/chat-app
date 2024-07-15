3c17da673b7057310e6063981824af8a832f637f
Mikhail <mikhailwahib1@gmail.com>
2024-07-15 17:05:44 +0300

    client: add env var for ws base url

--- client/src/utils/connectWs.ts
+++ client/src/utils/connectWs.ts
@@ -1,6 +1,6 @@
 export const connectWs = (roomId: string, userName: string) => {
 	const socket = new WebSocket(
-		`${import.meta.env.VITE_API_BASE_URL}/ws/${roomId}?username=${userName}`
+		`${import.meta.env.VITE_WS_BASE_URL}/ws/${roomId}?username=${userName}`
 	)
 
 	return socket
