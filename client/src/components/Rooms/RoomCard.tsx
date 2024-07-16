94e5e36b856eadf4b0a9fd0b172434affc9ff63d
Mikhail <mikhailwahib1@gmail.com>
2024-07-16 17:49:52 +0300

    client: fix cards overflow in rooms page

--- client/src/components/Rooms/RoomCard.tsx
+++ client/src/components/Rooms/RoomCard.tsx
@@ -2,15 +2,15 @@ import { Link } from 'react-router-dom'
 import { Room } from '../../types'
 
 const RoomCard = ({ room }: { room: Room }) => {
-	return (
-		<Link to={`/room/${room.id}`}>
-			<div className='relative w-52 p-2 border border-gray-600 rounded hover:shadow-2xl hover:shadow-white/15 hover:scale-[1.01] transition-all duration-200'>
-				<span className='absolute top-3 right-2 w-2 h-2 bg-green-500 rounded-full'></span>
-				<div>Name: {room.name}</div>
-				<div>Members: {room.members.length}</div>
-			</div>
-		</Link>
-	)
+  return (
+    <Link to={`/room/${room.id}`}>
+      <div className="relative w-52 h-full p-2 border border-gray-600 rounded hover:shadow-2xl hover:shadow-white/15 hover:scale-[1.01] transition-all duration-200">
+        <span className="absolute top-3 right-2 w-2 h-2 bg-green-500 rounded-full"></span>
+        <div>Name: {room.name}</div>
+        <div>Members: {room.members.length}</div>
+      </div>
+    </Link>
+  )
 }
 
 export default RoomCard
--- client/src/pages/Rooms.tsx
+++ client/src/pages/Rooms.tsx
@@ -3,29 +3,29 @@ import RoomCard from '../components/Rooms/RoomCard'
 import { Room } from '../types'
 
 const Rooms = () => {
-	const [rooms, setRooms] = useState<Room[]>([])
+  const [rooms, setRooms] = useState<Room[]>([])
 
-	const getRooms = async () => {
-		const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/rooms`)
-		const data = await res.json()
+  const getRooms = async () => {
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/rooms`)
+    const data = await res.json()
 
-		setRooms(data)
-	}
+    setRooms(data)
+  }
 
-	useEffect(() => {
-		getRooms()
-	}, [])
+  useEffect(() => {
+    getRooms()
+  }, [])
 
-	return (
-		<>
-			<h2 className='text-2xl mb-5'>Online Rooms: </h2>
-			<div className='flex gap-5'>
-				{rooms?.map((room, i) => {
-					return <RoomCard key={i} room={room} />
-				})}
-			</div>
-		</>
-	)
+  return (
+    <>
+      <h2 className="text-2xl mb-5">Online Rooms: </h2>
+      <div className="flex flex-wrap gap-5">
+        {rooms?.map((room, i) => {
+          return <RoomCard key={i} room={room} />
+        })}
+      </div>
+    </>
+  )
 }
 
 export default Rooms
